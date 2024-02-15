e0c3405cfbdbf577fa77410477fe5c87067806c1
Vishesh <vishesh.gubrani@gmail.com>
2024-02-15 22:06:55 +0530

    Faf

--- backend/lala.tf
+++ backend/lala.tf
@@ -1,3 +1,15 @@
 required_providers {
   aws = ">= version"
 }
+chihch
+data template_cloudinit_config fd
+ {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "filename"
+    content_type = "text/cloud-config"
+    content      = ""
+  }
+}
